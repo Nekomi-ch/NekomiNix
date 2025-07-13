bf7628dcbea0a8518719622f09f3bb3cab945fcf
Nekomi Fujiwara <morvenhui@gmail.com>
2025-07-13 17:30:05 +0800

    btop theme made

--- home-manager/btop.nix
+++ home-manager/btop.nix
@@ -3,7 +3,7 @@
     enable = true;
 
     settings = {
-      color_theme = "Default";
+      color_theme = "NekomiNight";
       theme_background = false;
 			rounded_corners = true;
 			truecolor = true;
